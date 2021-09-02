e7376f015ac54092989eb0c39c9b5dee7455064a
Vadim Perfilyev <vadimperfilev@gmail.com>
2021-09-02 21:46:37 +0300

    Add project files.

---
+++ SimpleSharedPointer.cpp
@@ -0,0 +1,25 @@
+﻿#include "SimpleSharedPointer.h"
+#include <memory>
+using namespace std;
+
+int main()
+{
+	{
+		cout << "********** FIRST TEST *************" << endl;
+		double value{ 13.3 };
+		SharedPointer<double> ptr1(value);
+		auto ptr2 = ptr1;
+		cout << "ptr1.count = " << ptr1.getCount() << '\n'
+			 << "ptr2.count = " << ptr2.getCount() << endl;
+	}
+	cout << endl;
+	{
+		cout << "********** SECOND TEST *************" << endl;
+		SharedPointer<double> ptr1(new double(13.5));
+		SharedPointer<double>* ptr2 = new SharedPointer<double>(ptr1);
+		cout << "ptr1.count = " << ptr1.getCount() << '\n'
+			 << "ptr2.count = " << ptr2->getCount() << endl;
+		delete ptr2;
+	}
+	return 0;
+}
---
+++ SimpleSharedPointer.h
@@ -0,0 +1,86 @@
+﻿#pragma once
+#include <iostream>
+#include <atomic>
+
+template<typename T>
+class SharedPointer final
+{
+private:
+	uint64_t* refCount = nullptr;
+	T* _pointer = nullptr;
+
+	inline void cleanUp()
+	{
+		--(*refCount);
+		if (*refCount == 0)
+		{
+			delete ptr;
+			delete refCount;
+		}
+	}
+public:
+	/*default constructors*/
+	SharedPointer() = delete;
+	explicit SharedPointer(T* pointer) : 
+		_pointer(pointer), refCount(new uint64_t{1}) {};
+	explicit SharedPointer(const T& value) :
+		_pointer(new T{value}), refCount(new uint64_t{1}) {};
+	explicit SharedPointer(T&& pointer)
+	{
+		_pointer = new T(std::move(pointer));
+		refCount = new uint64_t{1};
+	}
+
+	/*copy semantics*/
+	// copy constructor
+	SharedPointer(const SharedPointer& anotherPtr)
+	{
+		this->_pointer = anotherPtr._pointer;
+		this->refCount = anotherPtr.refCount;
+		if (anotherPtr._pointer) ++(*this->refCount);
+	}
+	// copy assignment
+	SharedPointer& operator=(const SharedPointer& anotherObj)
+	{
+		cleanUp();
+		this->_pointer = anotherObj._pointer;
+		this->refCount = anotherObj.refCount;
+		++(*refCount);
+	}
+
+	/*move semantics*/
+	// move constructor
+	explicit SharedPointer(SharedPointer&& tempObject)
+	{
+		this->_pointer = tempObject._pointer;
+		this->refCount = tempObject.refCount;
+		tempObject._pointer = tempObject.refCount = nullptr;
+	}
+	// move assignment
+	SharedPointer& operator=(SharedPointer&& tempObject)
+	{
+		cleanUp();
+		this->_pointer = tempObject._pointer;
+		this->refCount = tempObject.refCount;
+		tempObject._pointer = tempObject.refCount = nullptr;
+	}
+
+	~SharedPointer()
+	{
+		cout << "Decrementing destructor!" << endl;
+		if (--(*refCount) == 0)
+		{
+			cout << "Memory free!" << endl;
+			delete refCount;
+			delete _pointer;
+		}
+	}
+
+	/*operators*/
+	operator bool()	const	{ return _pointer != nullptr };
+	T& operator*()	const	{ return *_pointer };
+	T* operator->()	const	{ return _pointer };
+
+	/*members*/
+	uint64_t getCount() const { return *refCount; }
+};
